e933c57ee4a58a0342ff631425d7b4185f4bc5be
Julius Marozas <marozas.julius@gmail.com>
2019-08-28 15:12:23 +0300

    XMonad & Taffybar

---
+++ xmonad/.xmonad/lib/EWMHFullscreen.hs
@@ -0,0 +1,31 @@
+module EWMHFullscreen
+  ( addEWMHFullscreen
+  )
+where
+
+import           Control.Monad                  ( join
+                                                , when
+                                                )
+import           Data.Maybe                     ( maybeToList )
+import           XMonad
+
+
+addNETSupported :: Atom -> X ()
+addNETSupported x = withDisplay $ \dpy -> do
+  r               <- asks theRoot
+  a_NET_SUPPORTED <- getAtom "_NET_SUPPORTED"
+  a               <- getAtom "ATOM"
+  liftIO $ do
+    sup <- join . maybeToList <$> getWindowProperty32 dpy a_NET_SUPPORTED r
+    when (fromIntegral x `notElem` sup) $ changeProperty32 dpy
+                                                           r
+                                                           a_NET_SUPPORTED
+                                                           a
+                                                           propModeAppend
+                                                           [fromIntegral x]
+
+addEWMHFullscreen :: X ()
+addEWMHFullscreen = do
+  wms <- getAtom "_NET_WM_STATE"
+  wfs <- getAtom "_NET_WM_STATE_FULLSCREEN"
+  mapM_ addNETSupported [wms, wfs]
