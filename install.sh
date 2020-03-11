757279e901e8d66cac653de1674b1810707466b0
Julius Marozas <marozas.julius@gmail.com>
2020-03-11 18:40:21 +0000

    Install starship

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 stow fish git
 
+# Install starship
+curl -fsSL https://starship.rs/install.sh | bash -s -- --yes
+
 # Install Fisher
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 fish -c "fisher"
