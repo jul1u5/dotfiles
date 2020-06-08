10175c711b3bf9daeb3e9fc308c06af1900fe2cb
Julius Marozas <marozas.julius@gmail.com>
2020-06-08 14:52:26 +0300

    Remove hostname in networking module

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -1,10 +1,14 @@
 {
   networking = {
-    hostName = "nixos";
     networkmanager.enable = true;
     # useDHCP = false;
     # wicd.enable = true;
 
     nameservers = [ "1.1.1.1" "1.0.0.1" ];
+
+    firewall.allowedTCPPortRanges = [{
+      from = 1716;
+      to = 1764;
+    }];
   };
 }
