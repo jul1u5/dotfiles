de283b3f4dd8896668f6eaaf7dbef6d49c28ab44
Julius Marozas <marozas.julius@gmail.com>
2020-06-08 14:51:00 +0300

    Update vscode configuration

--- nixos/pkgs/vscode/default.nix
+++ nixos/pkgs/vscode/default.nix
@@ -1,34 +1,15 @@
 { pkgs, ... }:
 
 let
-  all-hies =
-    import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master")
-    { };
+  vsliveshare = builtins.fetchTarball
+    "https://github.com/msteen/nixos-vsliveshare/tarball/master";
 in {
-  imports = [
-    ./extensions-hack.nix
-    (builtins.fetchTarball {
-      sha256 = "12riba9dlchk0cvch2biqnikpbq4vs22gls82pr45c3vzc3vmwq9";
-      url =
-        "https://github.com/msteen/nixos-vsliveshare/archive/e6ea0b04de290ade028d80d20625a58a3603b8d7.tar.gz";
-    })
-  ];
+  imports = [ ./extensions-hack.nix vsliveshare ];
 
-  environment.systemPackages = with pkgs; [
-    # vscode
-    vscode-with-extensions
-    (all-hies.unstableFallback.selection {
-      selector = p: { inherit (p) ghc865; };
-    })
-    (import (builtins.fetchTarball
-      "https://github.com/hercules-ci/ghcide-nix/tarball/master")
-      { }).ghcide-ghc865
-  ];
+  environment.systemPackages = with pkgs; [ vscode-with-extensions ];
 
   services.vsliveshare = {
     enable = true;
-    enableWritableWorkaround = true;
-    enableDiagnosticsWorkaround = true;
     extensionsDir = "/home/julius/.vscode/extensions";
   };
 
@@ -38,18 +19,4 @@ in {
     ms-vscode.cpptools
     ms-python.python
   ];
-
-  nixpkgs = {
-    # config = {
-    #   packageOverrides = pkgs: rec {
-    #     vscodePython = pkgs.vscode-with-extensions.override {
-    #       vscodeExtensions = vscode-extensions: with vscode-extensions; [
-    #         ms-vscode.cpptools
-    #         ms-vscode.python
-    #       ];
-    #     };
-    #   };
-    # };
-    latestPackages = [ "vscode" "vscode-extensions" ];
-  };
 }
