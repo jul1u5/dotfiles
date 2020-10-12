6161c6725292be0a0b0bd0ba1e7c1f202c21a8d8
Julius Marozas <marozas.julius@gmail.com>
2020-10-12 16:22:40 +0300

    Unpin mesa

--- nixos/modules/hardware.nix
+++ nixos/modules/hardware.nix
@@ -15,13 +15,6 @@
       enable = true;
       driSupport32Bit = true;
 
-      package = (import (pkgs.fetchzip {
-        name = "old-nixpkgs";
-        url =
-          "https://github.com/NixOS/nixpkgs/archive/0a11634a29c1c9ffe7bfa08fc234fef2ee978dbb.tar.gz";
-        sha256 = "0vj5k3djn1wlwabzff1kiiy3vs60qzzqgzjbaiwqxacbvlrci10y";
-      }) { }).mesa.drivers;
-
       # package = (pkgs.mesa.override {
       #   galliumDrivers = [ "nouveau" "virgl" "swrast" "iris" ];
       # }).drivers;
