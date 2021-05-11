67782778a5905392599d0efb6a27b0bea9aae7ed
Julius Marozas <marozas.julius@gmail.com>
2021-05-12 00:31:19 +0300

    nixos: Add gaming module

---
+++ nixos/modules/gaming.nix
@@ -0,0 +1,7 @@
+{ ... }:
+
+{
+  environment.variables.SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS = "0";
+
+  programs.steam.enable = true;
+}
--- nixos/modules/steam.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-
-{
-  environment = {
-    variables.SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS = "0";
-    systemPackages = with pkgs; [ steam ];
-  };
-}
