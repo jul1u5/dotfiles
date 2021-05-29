f11aac62bad92c3ffa776b339243cde4a331666c
Julius Marozas <marozas.julius@gmail.com>
2021-05-29 23:39:46 +0300

    nixos: Add nodejs & rnix-lsp packages for Neovim

--- nixos/modules/neovim.nix
+++ nixos/modules/neovim.nix
@@ -1,9 +1,16 @@
+{ pkgs, ... }:
+
 {
   programs.neovim = {
     enable = true;
-    
+
     configure.customRC = ''
       source ~/.config/nvim/init.vim
     '';
   };
+
+  environment.systemPackages = with pkgs; [
+    nodejs
+    rnix-lsp
+  ];
 }
