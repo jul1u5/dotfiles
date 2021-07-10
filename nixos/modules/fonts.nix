1c33e00f2ce523b582d256de7e82763b431dcc2c
Julius Marozas <marozas.julius@gmail.com>
2021-07-11 00:30:50 +0300

    nixos: Fix XeTeX font loading issue

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -31,6 +31,18 @@ in
         monospace = [ "JetBrainsMono Nerd Font" "FiraCode Nerd Font" "Fira Code Symbol" "DejaVu Serif" ];
         emoji = [ "Twitter Color Emoji" ];
       };
+
+      # Reject WOFF fonts: "XeTeX chokes if fontconfig returns a WOFF file to them".
+      # See https://gitlab.freedesktop.org/fontconfig/fontconfig/-/issues/92
+      localConf = ''
+        <fontconfig>
+          <selectfont>
+            <rejectfont>
+              <glob>*.woff</glob>
+            </rejectfont>
+          </selectfont>
+        </fontconfig>
+      '';
     };
   };
 }
